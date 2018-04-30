9d621d05fca18deaf33e5f565c5e0f0b53aca3ad
francoisa <andre.francois@gmail.com>
2018-04-30 08:22:29 -0400

    Try new webpack.

--- src/server/server.js
+++ src/server/server.js
@@ -3,12 +3,33 @@ const graphQLHTTP = require('express-graphql');
 const { buildSchema } = require('graphql');
 const jwt = require('express-jwt');
 const schema = require('./todolistSchema').TodoSchema;
+const serve = require('webpack-serve');
 
-const app = express();
-app.use('/graphql', graphQLHTTP({
+const APP_PORT = 3000;
+const GRAPHQL_PORT = 8080;
+
+const handleNonRoot = function (req, res, next) {
+  if (req && req.path === '/favicon.ico') {
+    res.sendFile(path.resolve(__dirname, 'public', 'favicon.ico'));
+  }
+  else if (next) {
+    next();
+  }
+}
+
+
+const graphQLServer = express();
+graphQLServer.use('/graphql', graphQLHTTP({
     schema: schema,
     graphiql: true
 }));
 
-app.listen(4000);
+graphQLServer.listen(GRAPHQL_PORT);
 console.log('App listening on port 4000');
+
+const config = require('./webpack.config.js');
+
+serve({ config }).then((server) => {
+      assert(server);
+  setTimeout(() => server.close(done), 1000);
+});
